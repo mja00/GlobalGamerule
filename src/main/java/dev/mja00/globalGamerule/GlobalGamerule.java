53485ddb8d7cffcf143c16961a863ad4c6e0a790
mja00 <teamnirvana2@gmail.com>
2024-12-25 19:44:59 -0500

    Initial commit

---
+++ src/main/java/dev/mja00/globalGamerule/GlobalGamerule.java
@@ -0,0 +1,17 @@
+package dev.mja00.globalGamerule;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GlobalGamerule extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
