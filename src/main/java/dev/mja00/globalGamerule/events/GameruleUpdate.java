37492de77a8848114af17105d354f477a3d184f1
mja00 <teamnirvana2@gmail.com>
2024-12-25 20:19:02 -0500

    fix: clean up code

--- src/main/java/dev/mja00/globalGamerule/events/GameruleUpdate.java
+++ src/main/java/dev/mja00/globalGamerule/events/GameruleUpdate.java
@@ -3,6 +3,7 @@ package dev.mja00.globalGamerule.events;
 import dev.mja00.globalGamerule.GlobalGamerule;
 import io.papermc.paper.event.world.WorldGameRuleChangeEvent;
 import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.GameRule;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
@@ -29,14 +30,8 @@ public class GameruleUpdate implements Listener {
 
         plugin.getComponentLogger().info("Gamerule {} changed to {} by {}", gameRule.getName(), newValue, senderName);
         // Get all loaded worlds for the server and update the game rule for each world
-        for (String worldName : GlobalGamerule.getPlugin(GlobalGamerule.class).getServer().getWorlds().stream().map(WorldInfo::getName).toList()) {
-            plugin.getComponentLogger().info("Updating gamerule {} for world {}", gameRule.getName(), worldName);
-            // Get the gamerule type and cast it to the correct type
-            World world = plugin.getServer().getWorld(worldName);
-            if (world == null) {
-                plugin.getComponentLogger().warn("World {} not found, skipping update", worldName);
-                continue;
-            }
+        for (World world : Bukkit.getWorlds()) {
+            plugin.getComponentLogger().info("Updating gamerule {} for world {}", gameRule.getName(), world.getName());
             world.setGameRuleValue(gameRule.getName(), newValue);
         }
     }
